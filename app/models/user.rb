bfa01e0c6c1b42a5ee5c7217d396d01371210326
migs-b <m.bacho104@gmail.com>
2022-03-26 22:34:21 +0000

    W4D1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,7 @@
-def yellow_pager(word)
-    if word.length != 10
-        false 
-    else
-        phone_num = ""
+class User < ActiveRecord::Base
 
-        word.downcase.each_char do |letter|
-            if letter == "a" || letter == "b" || letter == "c"
-            phone_num += "2"
-            elsif letter == "d" || letter == "e" || letter == "f"
-            phone_num += "3"
-            elsif letter == "g" || letter == "h" || letter == "i"
-            phone_num += "4"
-            elsif letter == "j" || letter == "k" || letter == "l"
-            phone_num += "5"
-            elsif letter == "m" || letter == "n" || letter == "o"
-            phone_num += "6"
-            elsif letter == "p" || letter == "q" || letter == "r"
-            phone_num += "7"
-            elsif letter == 
-        end
+    has_many :finstagram_posts
+    has_many :comments
+    has_many :likes
 
-        phone_num
-    end
 end
