24e44ce8f6f12e70a9491d51d75076a4b2106494
migs-b <m.bacho104@gmail.com>
2022-04-02 22:31:09 +0000

    W5D1

--- app/actions.rb
+++ app/actions.rb
@@ -1,6 +1,36 @@
+helpers do
+    def current_user
+      User.find_by(id: session[:user_id])
+    end
+end
+
 get '/' do
     @finstagram_posts = FinstagramPost.order(created_at: :desc)
     erb(:index)
+end
+
+get '/login' do
+    erb(:login)
+end
+
+post '/login' do
+    username = params[:username]
+    password = params[:password]
+
+    user = User.find_by(username: username)  
+
+    if user && user.password == password
+        session[:user_id] = user.id
+        redirect to('/')
+    else
+        @error_message = "Login failed."
+        erb(:login)
+    end
+end
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect to('/')
 end
 
 get '/signup' do
@@ -13,12 +43,13 @@ post '/signup' do
     avatar_url = params[:avatar_url]
     username   = params[:username]
     password   = params[:password]
-
+  
     @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
-    
+  
     if @user.save
-       "User #{username} saved!"
+      redirect to('/login')
     else
-        erb(:signup)
+      erb(:signup)
     end
 end
+  
